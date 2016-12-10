c39e5253f73e92088b5a3f72919d3861579c3509
vivekkumar1 <vkumarpatna@gmail.com>
2016-12-10 06:27:06 +0000

    login.js edited online with Bitbucket

--- server/boot/login.js
+++ server/boot/login.js
@@ -13,50 +13,70 @@ router.post('/login', function (req, res) {
         "email": req.body.email,
         "password": req.body.password
     }
+    
+    
 
-    UserModel.login(userCredentials, 'user', function (err, result) {
-        if (err) {
+   UserModel.login(userCredentials, 'user', function (err, result) {			
+			if (err) {
+				//custom logger
+			
+				 res.json({message:"User Not Found"});
+				return;
+			}
+ 
+			
+			
+			//transform response to only return the token and ttl
+			
+       
+       
+      
+       
+    UserModel.find({where:{email:req.body.email}},{ fields: {email: true, role: true} },function (err, instance) {
+         
+         if (err) {
             //custom logger
            
-            res.status(401).json({ "error": "login failed" });
+            res.status(401).json({ "error": "wrong pass" });
             return;
         }
-
-       
-        //transform response to only return the token and ttl
-        
-    })
-
-
-  
-    var Credentials;
-    
-     UserModel.find({where:{email:req.body.email}},{ fields: {email: true, role: true} },function (err, instance) {
+         
                             
           var data = instance;
-        
-           var Credentials = {
+         //res.json(instance);
+           /*var Credentials = {
                
-               role: data[0].role,
-               email:data[0].email
+               role:instance[0].role,
+               email:instance[0].email
                
-           }
+           }*/
+           //res.json(Credentials);
          
-         var token = jwt.sign(Credentials, server.get('superSecret'), {
+         var token = jwt.sign({role:instance[0].role,email:instance[0].email}, server.get('superSecret'), {
               
          });
-               console.log(token);
-       var tokdata = jwt.verify(token, server.get('superSecret'),  {
+             //res.json(token);  //console.log(token);
+         var tokdata = jwt.verify(token, server.get('superSecret'),  {
        
-           
+         
        
         });
-    
-           var role = tokdata.role;
-         console.log(tokdata);
-         res.send({role,token});
+    var role= tokdata.role;
+        res.json({role,token});
+          //var role = tokdata.role;
+        // console.log(tokdata);
+         //res.json(tokdata.role,token);
+   
+        
+         
     });
-           
+       
+     
+   
+		});
+
+   
+         
   
     
                       
