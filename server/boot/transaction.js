e8f2a550eeccfbbb8789f97986780d080f284c8b
Vivek Sharma <vkumarpatna@gmail.com>
2017-06-16 04:58:41 +0530

    demo

--- server/boot/transaction.js
+++ server/boot/transaction.js
@@ -1539,12 +1539,12 @@ module.exports = function (server) {
       var collection = db.collection('inventory');
       for (var i = 0; i < dataOld.length; i++) {
         var query = { $pull: { 'salesTransaction': { id: vochID } } }
-      //  var query1 = { $inc: { BALANCE: Number(dataOld[i].itemQty) } };
-        // collection.update({ _id: new mongodb.ObjectId(dataOld[i].id) }, query1, function (err, instance) {
-        //   if (instance) {
-        //     console.log(instance.result);
-        //   }
-        // });
+       var query1 = { $inc: { BALANCE: Number(dataOld[i].itemQty) } };
+        collection.update({ _id: new mongodb.ObjectId(dataOld[i].id) }, query1, function (err, instance) {
+          if (instance) {
+            console.log(instance.result);
+          }
+        });
         collection.update({ _id: new mongodb.ObjectId(dataOld[i].id) }, query, function (err, instance) {
           if (instance) {
             console.log(instance.result);
