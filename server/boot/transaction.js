85b59d47707d47994ecb0742feeed7bf873b07cb
Vivek Sharma <vkumarpatna@gmail.com>
2017-06-24 04:14:22 +0530

    demo

--- server/boot/transaction.js
+++ server/boot/transaction.js
@@ -1414,7 +1414,7 @@ module.exports = function (server) {
           var accountData = data.invoiceData.accountlineItem;
           var ledger = [];
           if(data.roundOff){
-            if(Math.abs(data.roundOff)>=0.5){
+            if(data.roundOff<0){
               var roundoff = Math.abs(data.roundOff);
                ledger.push({ accountName: roundOffId, date: data.date, particular: data.invoiceData.consigneeAccountId, refNo: data.vochNo, voType: data.type, debit: Number(roundoff), voRefId: instance.id, isUo: false, visible: false, compCode: data.compCode })
             }else{
